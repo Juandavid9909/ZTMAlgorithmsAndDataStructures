8d70596d83b49c1d0bf76794b650438c7d17b98b
Juan David <juandavid918@gmail.com>
2023-03-24 14:14:20 -0500

    Sorting section finished

---
+++ 10_searching/linearSearch.js
@@ -0,0 +1,17 @@
+const beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
+
+// Retorna el index
+// beasts.indexOf("Godzilla");
+
+// Retorna el index
+// beasts.findIndex((item) => {
+//     return item === "Godzilla";
+// });
+
+// Retorna el elemento
+// beasts.find((item) => {
+//     return item === "Godzilla";
+// });
+
+// Retorna un booleano
+// beasts.includes("Godzilla");
